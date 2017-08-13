ac3a25a3744f3290b05fa9133ae79a4ba23c1139
Mehmet Mert Yildiran <mehmetmertyildiran@gmail.com>
2017-08-14 02:27:51 +0300

    Add arithmetic operations

--- swarm.py
+++ swarm.py
@@ -4,7 +4,9 @@ class Program:
 
     def __init__(self, filename):
         self.variable = []
-        self.variable_pointer = -1
+        self.pointer1 = -1
+        self.pointer2 = -2
+        self.power_of_ten = 0
 
         with open(filename) as f:
             program = ''.join(line.strip() for line in f)
@@ -23,21 +25,61 @@ class Program:
     def x1(self):
         print self.variable
 
-    # Assign an integer value to the current variable
+    # Assign an integer value
     def x2(self):
-        self.variable[self.variable_pointer] = 0
+        self.variable[self.pointer1] = 0
 
-    # Assign a float value to the current variable
+    # Assign a float value
     def x3(self):
-        self.variable[self.variable_pointer] = 0.0
+        self.variable[self.pointer1] = 0.0
 
-    # Assigne a string value to the current variable
+    # Assigne a string value
     def x4(self):
-        self.variable[self.variable_pointer] = ""
+        self.variable[self.pointer1] = ""
 
     # + 1
     def x5(self):
-        self.variable[self.variable_pointer] = self.variable[self.variable_pointer] + 1
+        self.variable[self.pointer1] = self.variable[self.pointer1] + 1 * (10 ** self.power_of_ten)
+
+    # - 1
+    def x6(self):
+        self.variable[self.pointer1] = self.variable[self.pointer1] - 1 * (10 ** self.power_of_ten)
+
+    # power of ten + 1
+    def x7(self):
+        self.power_of_ten = self.power_of_ten + 1
+
+    # power of ten - 1
+    def x8(self):
+        self.power_of_ten = self.power_of_ten - 1
+
+    # addition
+    def x9(self):
+        self.variable[self.pointer1] = self.variable[self.pointer1] + self.variable[self.pointer2]
+
+    # subtraction
+    def xA(self):
+        self.variable[self.pointer1] = self.variable[self.pointer1] - self.variable[self.pointer2]
+
+    # multiplication
+    def xB(self):
+        self.variable[self.pointer1] = self.variable[self.pointer1] * self.variable[self.pointer2]
+
+    # division
+    def xC(self):
+        self.variable[self.pointer1] = self.variable[self.pointer1] / self.variable[self.pointer2]
+
+    # modulus
+    def xD(self):
+        self.variable[self.pointer1] = self.variable[self.pointer1] % self.variable[self.pointer2]
+
+    # exponent
+    def xE(self):
+        self.variable[self.pointer1] = self.variable[self.pointer1] ** self.variable[self.pointer2]
+
+    # floor division
+    def x9(self):
+        self.variable[self.pointer1] = self.variable[self.pointer1] // self.variable[self.pointer2]
 
 if __name__ == "__main__":
     Program(sys.argv[1])
