95eb1b27bb9f81d3553b962dfee66a428b5a8d4e
Mehmet Mert Yildiran <mehmetmertyildiran@gmail.com>
2017-08-13 23:37:08 +0300

    Add a sample RNA program file and related lines to read it

--- rna.py
+++ rna.py
@@ -1,4 +1,14 @@
+import sys
+
 class Ribosome:
 
-    def __init__(self):
+    def __init__(self, filename):
         self.variables = []
+
+        with open(filename) as f:
+            program = ''.join(line.strip() for line in f)
+            print program
+
+
+if __name__ == "__main__":
+    Ribosome(sys.argv[1])
